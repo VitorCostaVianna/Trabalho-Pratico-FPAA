1f7326ab704068fa9f5eca43090b19b62f2d2212
Guilherme Vieira <guilhermearv3@gmail.com>
2026-03-28 00:56:41 -0300

    kruskal development

--- tp1-dsu-kruskal/src/model/Edge.java
+++ tp1-dsu-kruskal/src/model/Edge.java
@@ -1,1 +1,26 @@
-// TODO - GUILHERME
+package model;
+
+public class Edge<T> {
+    private Node<T> source;
+    private Node<T> destination;
+    private int weight;
+
+    public Edge(Node<T> source, Node<T> destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Node<T> getSource(){
+        return this.source;
+    }
+
+    public Node<T> getDestination(){
+        return this.destination;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+
+}
--- tp1-dsu-kruskal/src/model/Graph.java
+++ tp1-dsu-kruskal/src/model/Graph.java
@@ -1,1 +1,33 @@
-// TODO - GUILHERME
+package model;
+
+import java.util.Arrays;
+
+public class Graph<T, K> {
+    private Node<T>[] nodes;
+    private Edge<K>[] edges;
+
+    public Graph(Node<T>[] nodes, Edge<K>[] edges){
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public Node<T>[] getNodes(){
+        return this.nodes;
+    }
+
+    public Edge<K>[] getEdges(){
+        return this.edges;
+    }
+
+    public Edge<K>[] getSortedEdges(){
+        Edge<K>[] clonedEdge = this.edges.clone();
+        Arrays.sort(clonedEdge, (a, b) -> Integer.compare(a.getWeight(), b.getWeight()));
+        return clonedEdge;
+    }
+
+    public void addNode(Node<T> node){
+        this.nodes = Arrays.copyOf(this.nodes, this.nodes.length + 1);
+        this.nodes[this.nodes.length - 1] = node;
+    }
+
+}

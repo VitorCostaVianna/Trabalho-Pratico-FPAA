e4c29e36c46be86377eb44a149d47e319c73e6a6
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-03-28 13:22:38 -0300

    feat: implement TarjanDSU class with find and union methods

--- tp1-dsu-kruskal/src/ds/TarjanDSU.java
+++ tp1-dsu-kruskal/src/ds/TarjanDSU.java
@@ -1,7 +1,58 @@
-// TODO - VITOR
-
 package ds;
 
-public class TarjanDSU {
-    
+public class TarjanDSU implements DisjointSet {
+    private int[] parent;
+    private int[] rank;
+    private long memoryAccesses;
+
+    public TarjanDSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        memoryAccesses = 0;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    @Override
+    public int find(int i) {
+        memoryAccesses++;
+        if (parent[i] != i) {
+            memoryAccesses++;
+            parent[i] = find(parent[i]); 
+            memoryAccesses++;
+        }
+        
+        memoryAccesses++;
+        return parent[i];
+    }
+
+    @Override
+    public void union(int i, int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+
+        if (rootI != rootJ) {
+            memoryAccesses += 2; 
+            
+            if (rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+                memoryAccesses++; 
+            } else {
+                parent[rootI] = rootJ;
+                memoryAccesses++; 
+                
+                if (rank[rootI] == rank[rootJ]) {
+                    rank[rootJ]++;
+                    memoryAccesses += 2; 
+                }
+            }
+        }
+    }
+
+    @Override
+    public long getMemoryAccesses() {
+        return memoryAccesses;
+    }
 }

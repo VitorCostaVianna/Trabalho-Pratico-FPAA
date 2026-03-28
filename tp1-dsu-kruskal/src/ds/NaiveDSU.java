6fa8e7749747734fc6b279f7dd457df5bf3ff738
VitorCostaVianna <vcvianna@sga.pucminas.br>
2026-03-28 13:22:20 -0300

    feat: implement NaiveDSU class with find and union methods

--- tp1-dsu-kruskal/src/ds/NaiveDSU.java
+++ tp1-dsu-kruskal/src/ds/NaiveDSU.java
@@ -1,1 +1,41 @@
-// TODO - VITOR
+package ds;
+
+public class NaiveDSU implements DisjointSet {
+    private int[] parent;
+    private long memoryAccesses;
+
+    public NaiveDSU(int n) {
+        parent = new int[n];
+        memoryAccesses = 0;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; 
+        }
+    }
+
+    @Override
+    public int find(int i) {
+        memoryAccesses++;
+        if (parent[i] == i) {
+            return i;
+        }
+        
+        memoryAccesses++;
+        return find(parent[i]); 
+    }
+
+    @Override
+    public void union(int i, int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+
+        if (rootI != rootJ) {
+            parent[rootI] = rootJ;
+            memoryAccesses++;
+        }
+    }
+
+    @Override
+    public long getMemoryAccesses() {
+        return memoryAccesses;
+    }
+}
